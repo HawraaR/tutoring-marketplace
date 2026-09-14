7cb634643d00b3243f013af6b4d87c8802439fe5
Hawraa R. <Hawraa.ramadan.02@gmail.com>
2026-09-14 23:15:31 +0300

    Merge branch 'main' of https://github.com/HawraaR/tutoring-marketplace

--- frontend/src/components/sessions/SessionRow.tsx
+++ frontend/src/components/sessions/SessionRow.tsx
@@ -2,7 +2,6 @@
 import { CheckCircle2, Clock3, MessageCircle, MoreHorizontal, Video } from "lucide-react";
 import { CourseLabel } from "../../components/dashboard/CourseLabel";
 import { STATUS_STYLES } from "../../lib/utils/sessionHelpers";
-import type {  UnifiedSession } from "../../types";
 import { useState } from "react";
 import { ReviewModal } from "./ReviewModel";
 import type { UnifiedSession } from "../../types";
