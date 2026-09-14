9fae2b5d09e3b84427b4fe15cb82430358da91c5
Hawraa R. <Hawraa.ramadan.02@gmail.com>
2026-09-14 23:20:07 +0300

    update

--- frontend/src/components/sessions/SessionRow.tsx
+++ frontend/src/components/sessions/SessionRow.tsx
@@ -6,6 +6,7 @@ import type { UnifiedSession } from "../../types";
 import { SessionReviewEntry } from "./sessionReviewEntry";
 import { isSessionPast } from "../review/reviewCriteria";
 
+
 interface SessionRowProps {
   session: UnifiedSession;
   isTutorMode: boolean;
